6f61e7e15f2d6e38a794c4c39c712f63ccda97a4
MOHAMMED SHIBIN V <72612100+MohdShibin@users.noreply.github.com>
2021-04-24 22:21:41 +0530

    created avatar.dart to Refactor

---
+++ lib/widgets/avatar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Avatar extends StatelessWidget{
+  final String displayImage;
+
+  Avatar({
+    @required this.displayImage,
+});
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 5,right: 5),
+          child: ClipRRect(
+            child: Image.asset(displayImage, width: 50, height: 50),
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 1.0,
+          child:Container(
+            width: 15,
+            height: 15,
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+}
