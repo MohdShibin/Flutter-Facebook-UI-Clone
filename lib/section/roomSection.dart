d88d05fab056725ec3bfe9132498e6b0e1e8fd2d
MOHAMMED SHIBIN V <72612100+MohdShibin@users.noreply.github.com>
2021-04-24 22:17:51 +0530

    made createRoom section

---
+++ lib/section/roomSection.dart
@@ -0,0 +1,43 @@
+import 'package:fb/widgets/avatar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoomSection extends StatelessWidget{
+
+  Widget createRoomButtton() {
+    return Container(
+      padding: EdgeInsets.only(left: 5,right: 5),
+      child: OutlineButton.icon(
+        shape: StadiumBorder(),
+        borderSide: BorderSide(color: Colors.blue[100], width: 2),
+        onPressed: () {
+          print("chatroom");
+        },
+        icon: Icon(Icons.video_call, color: Colors.purple,),
+        label: Text("Create \nRoom", style: TextStyle(color: Colors.blue),),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      child: ListView(
+
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.all(10),
+        children: [
+          createRoomButtton(),
+          Avatar(displayImage: "assets/dq.jpg"),
+          Avatar(displayImage: "assets/tovino.jpg"),
+          Avatar(displayImage: "assets/dq.jpg"),
+          Avatar(displayImage: "assets/tovino.jpg"),
+          Avatar(displayImage: "assets/dq.jpg"),
+          Avatar(displayImage: "assets/tovino.jpg"),
+        ],
+      ),
+    );
+  }
+
+}
